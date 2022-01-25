8402504ee4b0dd23e58b2e44d6f04c0fa2a30e12
IMBABOT1 <zlovred12@gmail.com>
2022-01-25 16:23:53 +0300

    temp

--- core/src/com/imbabot/snake_game/Apple.java
+++ core/src/com/imbabot/snake_game/Apple.java
@@ -14,7 +14,8 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Apple {
 
-    private Vector2 position;
+    private int cellX;
+    private int cellY;
     private TextureRegion textureApple;
 
     private boolean isActive;
@@ -23,9 +24,6 @@ public class Apple {
     private int topBorder;
 
 
-    public Vector2 getPosition() {
-        return position;
-    }
 
     public void setActive(boolean active) {
         isActive = active;
@@ -34,20 +32,19 @@ public class Apple {
     public Apple(TextureAtlas atlas){
         this.textureApple = atlas.findRegion("Apple");
         this.isActive = false;
-        this.position = new Vector2(0, 0);
+        this.cellX = MathUtils.random(0, 10);
+        this.cellX = MathUtils.random(0, 10);
         this.leftBorder = 30;
         this.topBorder = 780;
-        this.position.x = MathUtils.random(leftBorder, topBorder);
-        this.position.y = MathUtils.random(leftBorder, topBorder);
         this.offset = 40;
     }
 
     public void render(SpriteBatch batch){
         if (isActive) {
-            batch.draw(textureApple, position.x - offset, position.y - offset);
+            batch.draw(textureApple, cellX * GameMap.CELL_SIZE, cellY * GameMap.CELL_SIZE);
         }else {
-            position.x = MathUtils.random(leftBorder, topBorder);
-            position.y = MathUtils.random(leftBorder, topBorder);
+            cellX = MathUtils.random(0, 10);
+            cellY = MathUtils.random(0, 10);
             isActive = true;
         }
     }
